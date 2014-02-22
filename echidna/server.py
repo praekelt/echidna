c226350d6b01a3f2633a3a07be9060321d86c5a8
Simon Cross <hodgestar@gmail.com>
2014-02-23 01:02:34 +0200

    Skeleton of Echidna server.

---
+++ echidna/server.py
@@ -0,0 +1,33 @@
+from cyclone.web import Application, RequestHandler
+from cyclone.websocket import WebSocketHandler
+
+
+class EchidnaServer(Application):
+    def __init__(self, **settings):
+        handlers = [
+            (r"/", RootHandler),
+            (r"/publish", PublicationHandler),
+            (r"/subscribe", SubscriptionHandler),
+        ]
+        super(EchidnaServer, self).__init__(self, handlers, **settings)
+
+
+class RootHandler(RequestHandler):
+    def get(self):
+        self.render("demo.html")
+
+
+class PublicationHandler(RequestHandler):
+    def post(self):
+        pass
+
+
+class SubscriptionHandler(WebSocketHandler):
+    def connectionMade(self, *args, **kw):
+        pass
+
+    def connectionLost(self, reason):
+        pass
+
+    def messageReceived(self, msg):
+        self.sendMessage("foo")
