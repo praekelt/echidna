a5e4a46fec449abe168614639624c3785fd9bf4c
hedleyroos <hedleyroos@gmail.com>
2014-09-09 16:41:57 +0200

    Refactor so RedisChannel subclasses InMemoryChannel

--- echidna/cards/memory_store.py
+++ echidna/cards/memory_store.py
@@ -51,42 +51,6 @@ class InMemoryChannel(object):
             del self._clients[client.client_id]
 
     def cards(self, client=None):
-        return self._cards
-
-    def publish(self, card):
-        self._cards.append(card)
-        for client in self._clients.itervalues():
-            client.publish(self.name, card)
-
-
-class RedisChannel(object):
-    """
-    A channel in an :class:`InMemoryCardStore`.
-    """
-
-    implements(IRedisChannel)
-
-    def __init__(self, name):
-        self.name = name
-        self._clients = {}
-        self._redis = redis.Redis("localhost")
-        self._cards = []
-        self._key = "echidna%scards" % self.name
-        values = self._redis.lrange(self._key, 0, -1)
-        try:
-            self._cards = [json.loads(v) for v in values]
-        except ValueError:
-            pass
-
-    def subscribe(self, client, last_seen=None):
-        client.last_seen = last_seen
-        self._clients[client.client_id] = client
-
-    def remove(self, client):
-        if client.client_id in self._clients:
-            del self._clients[client.client_id]
-
-    def cards(self, client=None):
         if (client is not None) and (client.last_seen is not None):
             last_seen_cards = []
             for card in self._cards:
@@ -98,9 +62,30 @@ class RedisChannel(object):
 
     def publish(self, card):
         self._cards.append(card)
-        self._redis.rpush(self._key, json.dumps(card))
         for client in self._clients.itervalues():
             client.publish(self.name, card)
+
+
+class RedisChannel(InMemoryChannel):
+    """
+    A channel in an :class:`InMemoryCardStore`.
+    """
+
+    implements(IRedisChannel)
+
+    def __init__(self, name):
+        super(RedisChannel, self).__init__(name)
+        self._redis = redis.Redis("localhost")
+        self._key = "echidna%scards" % self.name
+        values = self._redis.lrange(self._key, 0, -1)
+        try:
+            self._cards = [json.loads(v) for v in values]
+        except ValueError:
+            pass
+
+    def publish(self, card):
+        self._redis.rpush(self._key, json.dumps(card))
+        super(RedisChannel, self).publish(card)
 
 
 class InMemoryCardStore(object):
