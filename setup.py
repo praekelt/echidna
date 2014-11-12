bfc44810784c10fbef4c468be9942bb5f952d033
hedleyroos <hedleyroos@gmail.com>
2014-11-12 14:09:44 +0200

    Up version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="echidna",
-    version="0.0.10",
+    version="0.0.12",
     url='http://github.com/praekelt/echidna',
     license='BSD',
     description='A scalable pub-sub WebSocket service.',
